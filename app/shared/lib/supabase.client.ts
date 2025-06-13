1b7accea023e06a23c25b2a308ad10738d96a78d
emmyrlim <emmyrlim@gmail.com>
2025-06-12 19:13:21 -0700

    attempt env fix

--- app/shared/lib/supabase.client.ts
+++ app/shared/lib/supabase.client.ts
@@ -1,8 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-export function createClient() {
-  return createBrowserClient(
-    import.meta.env.VITE_SUPABASE_URL!,
-    import.meta.env.VITE_SUPABASE_ANON_KEY!
-  );
+export function createClient(env: {
+  SUPABASE_URL: string;
+  SUPABASE_ANON_KEY: string;
+}) {
+  return createBrowserClient(env.SUPABASE_URL!, env.SUPABASE_ANON_KEY!);
 }
